5f6653cfa33e8b791ed88c0bb520b7420f476897
Meagan Blais <meaganblais@gmail.com>
2018-03-08 16:27:49 -0500

    setting up Blockchain

--- main.js
+++ main.js
@@ -14,3 +14,30 @@ class Block {
     return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
   }
 }
+
+class Blockchain {
+  constructor(){
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, '01/01/2018', 'Genesis block', '0');
+  }
+
+  getLatestBlock(){
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let randomCoin = new Blockchain();
+randomCoin.addBlock(new Block(1, "17/02/2018", { amount: 4}));
+randomCoin.addBlock(new Block(2, "26/02/2018", { amount: 10}));
+
+
+console.log(JSON.stringify(randomCoin, null, 4))
